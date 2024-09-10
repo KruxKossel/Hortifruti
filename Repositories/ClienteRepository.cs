e80efd0ccfba6de9dbd6d4bb90f79ece4be3cef7
Rafael Thomaz <rafael.hthomaz123@gmail.com>
2024-09-10 19:30:33 -0300

    new commit

--- Repositories/ClienteRepository.cs
+++ Repositories/ClienteRepository.cs
@@ -22,7 +22,8 @@ namespace Hortifruti.Repositories
                 {
 
                     // add
-                    using (var comando = connection.DbConnection().CreateCommand()){
+                    using (var comando = connection.DbConnection().CreateCommand())
+                    {
 
                         comando.CommandText = "INSERT INTO clientes (Nome, CPF, Telefone) VALUES (@nome, @cpf, @telefone)";
                         comando.Parameters.AddWithValue("@nome", entidade.Nome);
@@ -34,7 +35,6 @@ namespace Hortifruti.Repositories
                     Console.Clear();
                     Console.WriteLine("\nDados inseridos com sucesso!\n\n");
                     return true; // cliente adicionado com sucesso
-
                 }
 
             }
@@ -44,7 +44,8 @@ namespace Hortifruti.Repositories
                 return false;
 
             }
-            catch(Exception ex){
+            catch (Exception ex)
+            {
                 Console.WriteLine("Error geral:" + ex.Message);
                 return false;
             }
@@ -52,17 +53,134 @@ namespace Hortifruti.Repositories
 
         public List<Cliente> Atualizar()
         {
-            throw new NotImplementedException();
+           List<Cliente> cliente = new List<Cliente>();
+
+            Console.WriteLine("Digite o id:");
+            int id = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Digite a nova Nome:");
+            string nome = Console.ReadLine();
+
+            Console.WriteLine("Digite o novo CPF:");
+            string cpf = Console.ReadLine();
+
+            Console.WriteLine("Digite o novo telefone:");
+            string telefone = Console.ReadLine();
+
+            try
+            {
+                var connection = new HortifrutiContext(_connectionString);
+                using (connection.DbConnection())
+                {
+                    using (var comando = connection.DbConnection().CreateCommand())
+                    {
+                        comando.CommandText = "UPDATE clientes SET Nome = @nome, CPF = @cpf, Telefone = @telefone WHERE Id = @id";
+                        comando.Parameters.AddWithValue("@id", id);
+                        comando.Parameters.AddWithValue("@nome", nome);
+                        comando.Parameters.AddWithValue("@cpf", cpf);
+                        comando.Parameters.AddWithValue("@telefone", telefone);
+
+                        int linhasAfetadas = comando.ExecuteNonQuery();
+                        if (linhasAfetadas > 0)
+                        {
+                            Console.WriteLine("Dados atualizados com sucesso");
+                        }
+                        else
+                        {
+                            Console.WriteLine("Nenhum dado encontrado");
+                        }
+                    }
+                }
+            }
+            catch (SQLiteException sqLiteEx)
+            {
+                Console.WriteLine("Erro SQLite: " + sqLiteEx.Message);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Erro geral: " + ex.Message);
+            }
+            return cliente;
         }
+
+        
 
         public List<Cliente> Listar()
         {
-            throw new NotImplementedException();
+            List<Cliente> clientes = new List<Cliente>();
+            try
+            {
+                var connection = new HortifrutiContext(_connectionString);
+                using (connection.DbConnection())
+                {
+                    using (var comando = connection.DbConnection().CreateCommand())
+                    {
+                        comando.CommandText = "SELECT * FROM clientes";
+                        using (var leitor = comando.ExecuteReader())
+                        {
+                            while (leitor.Read())
+                            {
+                                Cliente cliente = new Cliente(
+                                    leitor["Nome"].ToString(),
+                                    leitor["Cpf"].ToString(),
+                                    leitor["Telefone"].ToString()
+                                );
+                                clientes.Add(cliente);
+                            }
+                        }
+                    }
+                }
+            }
+            catch (SQLiteException sqLiteEx)
+            {
+                Console.WriteLine("Error SQLite:" + sqLiteEx.Message);
+
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Error geral:" + ex.Message);
+            }
+
+            return clientes;
         }
 
         public bool Remover(Cliente entidade)
         {
-            throw new NotImplementedException();
+          try
+            {
+                 Console.WriteLine("Digite o ID do Cliente a ser removido: ");
+                 int id = int.Parse(Console.ReadLine());
+
+                var connection = new HortifrutiContext(_connectionString);
+                using (connection.DbConnection())
+                {
+                    using (var comando = connection.DbConnection().CreateCommand())
+                    {
+                        comando.CommandText = "DELETE FROM clientes WHERE Id = @id ";
+                        comando.Parameters.AddWithValue("@id",id);
+
+                        int linhasAfetadas = comando.ExecuteNonQuery();
+
+                        if(linhasAfetadas>0){
+                            Console.WriteLine("Cliente removido com sucesso!");
+                            return true;
+                        }else{
+                            Console.WriteLine("Cliente não encontrado");
+                            return false;
+                        }
+                    }
+                }
+            }
+           catch (SQLiteException sqLiteEx)
+            {
+                Console.WriteLine("Error SQLite:" + sqLiteEx.Message);
+                return false;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Error geral:" + ex.Message);
+                return false;
+            }
         }
     }
 }
