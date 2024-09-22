89122da8d9a7c10d928a2e674dc064536441f256
Ellen.Kos <143337580+KruxKossel@users.noreply.github.com>
2024-09-22 18:48:34 -0300

    Mano att em varias coisas
    interface, repositórios, services, routers e menus

--- Models/ItensVenda.cs
+++ Models/ItensVenda.cs
@@ -5,7 +5,7 @@ using System.Threading.Tasks;
 
 namespace Hortifruti.Models
 {
-    public class ItensVenda(int produtoId, int quantidade, decimal preco)
+    public class ItensVenda( int produtoId, int quantidade, decimal preco)
     {
         public int Id { get;}
         public int ProdutoId { get; } = produtoId;
--- Routers/ItensVendaRouter.cs
+++ Routers/ItensVendaRouter.cs
@@ -1,35 +1,35 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Hortifruti.Models;
-using Hortifruti.Repositories;
-using Hortifruti.Services;
+// using System;
+// using System.Collections.Generic;
+// using System.Linq;
+// using System.Threading.Tasks;
+// using Hortifruti.Models;
+// using Hortifruti.Repositories;
+// using Hortifruti.Services;
 
-namespace Hortifruti.Routers
-{
-    public class ItensVendaRouter(ItensVendaService itensVendaService) : ICrud<ItensVenda>
-    {
-        private readonly ItensVendaService _itensVendaService = itensVendaService;
+// namespace Hortifruti.Routers
+// {
+//     public class ItensVendaRouter(ItensVendaService itensVendaService) : ICrud<ItensVenda>
+//     {
+//         private readonly ItensVendaService _itensVendaService = itensVendaService;
 
-        public (bool, decimal) Adicionar(ItensVenda entidade)
-        {
-            return _itensVendaService.Adicionar(entidade);
-        }
+//         public (bool, decimal) Adicionar(ItensVenda entidade)
+//         {
+//             return _itensVendaService.Adicionar(entidade);
+//         }
 
-        public List<ItensVenda> Atualizar()
-        {
-            return _itensVendaService.Atualizar();
-        }
+//         public List<ItensVenda> Atualizar()
+//         {
+//             return _itensVendaService.Atualizar();
+//         }
 
-        public List<ItensVenda> Listar()
-        {
-            return _itensVendaService.Listar();
-        }
+//         public List<ItensVenda> Listar()
+//         {
+//             return _itensVendaService.Listar();
+//         }
 
-        public bool Remover(int id)
-        {
-            return _itensVendaService.Remover(id);
-        }
-    }
-}
+//         public bool Remover(int id)
+//         {
+//             return _itensVendaService.Remover(id);
+//         }
+//     }
+// }
